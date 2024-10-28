0e4c27d60110fb090a2571652d3aa65fa7a71e4a
brgltd <brgltdxyz@gmail.com>
2024-10-28 11:34:05 -0300

    feat: include missing delimeter component

--- frontend/original-game/models/Goal.tsx
+++ frontend/original-game/models/Goal.tsx
@@ -2,24 +2,13 @@ import { useBox } from "@react-three/cannon";
 import { useRef } from "react";
 import type { Mesh } from "three";
 
-// import type { BoxProps } from '@react-three/cannon'
+import type { BoxProps } from "@react-three/cannon";
 
-// export function Goal({ args = [1, 1, 1], ...props }: BoxProps): null {
-//   useBox(() => ({ isTrigger: true, args, userData: { trigger: true }, ...props }), undefined, [args, props])
-//   return null
-// }
-
-export function Goal({ args, ...props }: BoxProps) {
-  const [ref] = useBox(
-    () => ({ type: "Static", args, ...props }),
-    useRef<Mesh>(null),
+export function Goal({ args = [1, 1, 1], ...props }: BoxProps): null {
+  useBox(
+    () => ({ isTrigger: true, args, userData: { trigger: true }, ...props }),
+    undefined,
     [args, props],
   );
-
-  return (
-    <mesh castShadow receiveShadow ref={ref}>
-      <boxGeometry args={args} />
-      <meshStandardMaterial color="indianred" />
-    </mesh>
-  );
+  return null;
 }
