951f9aa54a8317e7d10d30652429e59e2e66d360
brgltd <brgltdxyz@gmail.com>
2024-11-04 15:52:45 -0300

    feat: boilerplate card page

---
+++ frontend/app/card/page.tsx
@@ -0,0 +1,26 @@
+const colors = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "orange",
+  "purple",
+  "white",
+  "black",
+  "gray",
+];
+
+export default function Card() {
+  return (
+    <div>
+      <div>Choose the color for your car</div>
+      <select>
+        {colors.map((color) => (
+          <option key={color} value={color}>
+            {color}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
