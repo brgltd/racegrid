e0bd068a3e4ce81d54f7eba1da93837c8b5a0608
brgltd <brgltdxyz@gmail.com>
2024-11-05 17:24:30 -0300

    feat: add userToTokenURI in RaceGridNFT

--- contracts/src/RaceGridNFT.sol
+++ contracts/src/RaceGridNFT.sol
@@ -4,25 +4,21 @@ pragma solidity ^0.8.26;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract RaceGridNFT is ERC721 {
-    error InsufficientFee(uint256 receivedValue);
-
     uint256 private constant FEE = 0.01e18;
 
-    uint256 public counter;
-    mapping(uint256 id => string uri) tokens;
+    uint256 private tokenId;
+
+    mapping(uint256 id => string uri) public tokenIdToURI;
+
+    mapping(address user => string uri) public userToTokenURI;
 
     constructor() ERC721("RaceGrid", "RCG") {}
 
     function mint(string memory uri) public payable {
-        if (msg.value < FEE) {
-            revert InsufficientFee(msg.value);
-        }
-        tokens[counter] = uri;
-        _safeMint(msg.sender, counter);
-        ++counter;
-    }
-
-    function tokenURI(uint256 id) public view override returns (string memory) {
-        return tokens[id];
+        require(msg.value >= FEE, "insufficient fee");
+        tokenIdToURI[tokenId] = uri;
+        userToTokenURI[msg.sender] = uri;
+        _safeMint(msg.sender, tokenId);
+        ++tokenId;
     }
 }
--- frontend/app/card/page.tsx
+++ frontend/app/card/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 const colors = [
   "red",
   "green",
@@ -11,18 +15,24 @@ const colors = [
 ];
 
 export default function Card() {
+  const [color, setColor] = useState("");
   return (
     <div>
       <div>Choose the color for your car</div>
-      <select>
-        {colors.map((color) => (
-          <option key={color} value={color}>
-            {color}
+      <select
+        value={color}
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+      >
+        {colors.map((colorItem) => (
+          <option key={colorItem} value={colorItem}>
+            {colorItem}
           </option>
         ))}
       </select>
       {/* 0.01 ETH ~30usd */}
-      {/* profit for winnint a track will be 0.002eth =  */}
+      {/* profit for winning a track will be 0.002eth = ~6usd */}
       <div>Cost: 0.01 ETH</div>
       <button>mint</button>
     </div>
