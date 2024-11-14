55fcbc8702d2fb9730a34c70dcfc3e672a3d4998
brgltd <brgltdxyz@gmail.com>
2024-11-14 10:15:25 -0300

    feat: border-top for active navbar item

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -11,22 +11,23 @@ export function Navbar() {
   const isHomepage = pathname === "/";
   const isMintPage = pathname === "/nft";
   const isLeaderboardPage = pathname === "/leaderboard";
-  const baseItemStyle = "mr-12 hover-glow hover:text-blue-400 transition-all";
-  const hoverItemStyle = "";
+  const baseItemStyle =
+    "mr-12 hover-glow hover:text-blue-400 transition-all border-t-2 border-transparent";
+  const activeItemStyle = "border-t-2 border-white";
   return isChallengePage ? null : (
     <nav>
       <ul className="flex lg:flex-row flex-col lg:items-center mt-6 mb-16 justify-center">
         <ul className="flex flex-row flex-wrap mb-8 lg:mb-0 gap-y-6">
-          <li className={cn(baseItemStyle, isHomepage ? hoverItemStyle : "")}>
+          <li className={cn(baseItemStyle, isHomepage ? activeItemStyle : "")}>
             <Link href="/">RACEGRID</Link>
           </li>
-          <li className={cn(baseItemStyle, isMintPage ? hoverItemStyle : "")}>
+          <li className={cn(baseItemStyle, isMintPage ? activeItemStyle : "")}>
             <Link href="/nft">MINT NFT</Link>
           </li>
           <li
             className={cn(
               baseItemStyle,
-              isLeaderboardPage ? hoverItemStyle : "",
+              isLeaderboardPage ? activeItemStyle : "",
             )}
           >
             <Link href="/leaderboard">LEADERBOARD</Link>
