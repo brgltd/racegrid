d2866af64a8f6b938c1db3a29892d531195ce20e
brgltd <brgltdxyz@gmail.com>
2024-11-14 09:24:09 -0300

    feat: render main controls in the game

--- frontend/racing-game-r3f/ui/Help.tsx
+++ frontend/racing-game-r3f/ui/Help.tsx
@@ -1,16 +1,74 @@
-import { useStore } from '../store'
+import { useStore } from "../store";
 
-import { Keys } from './Keys'
+import { Keys } from "./Keys";
 
 export function Help(): JSX.Element {
-  const [set, help, sound] = useStore((state) => [state.set, state.help, state.sound])
+  const [set, help, sound] = useStore((state) => [
+    state.set,
+    state.help,
+    state.sound,
+  ]);
 
   return (
     <>
-      <div className={`${sound ? 'sound' : 'nosound'}`}></div>
+      <div className="nosound"></div>
       <div className="help">
-        {!help && <button onClick={() => set({ help: true })}>i</button>}
-        <div className={`popup ${help ? 'open' : ''}`}>
+        {!help && (
+          <div className="row">
+            <button
+              onClick={() => set({ help: true })}
+              className="row-item control-key"
+            >
+              WASD
+            </button>
+            <div className="row-item control-text">Drive</div>
+          </div>
+        )}
+        {!help && (
+          <div className="row">
+            <button
+              onClick={() => set({ help: true })}
+              className="row-item control-key"
+            >
+              SPACE
+            </button>
+            <div className="row-item control-text">Break</div>
+          </div>
+        )}
+        {!help && (
+          <div className="row">
+            <button
+              onClick={() => set({ help: true })}
+              className="row-item control-key"
+            >
+              R
+            </button>
+            <div className="row-item control-text">Reset</div>
+          </div>
+        )}
+        {!help && (
+          <div className="row">
+            <button
+              onClick={() => set({ help: true })}
+              className="row-item control-key"
+            >
+              Q
+            </button>
+            <div className="row-item control-text">Quit</div>
+          </div>
+        )}
+        {!help && (
+          <div className="row">
+            <button
+              onClick={() => set({ help: true })}
+              className="row-item control-key"
+            >
+              I
+            </button>
+            <div className="row-item control-text">Help</div>
+          </div>
+        )}
+        <div className={`popup ${help ? "open" : ""}`}>
           <button className="popup-close" onClick={() => set({ help: false })}>
             i
           </button>
@@ -20,5 +78,5 @@ export function Help(): JSX.Element {
         </div>
       </div>
     </>
-  )
+  );
 }
