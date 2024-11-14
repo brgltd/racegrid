6d7f41f44bd93cf6fe2c63b2ff08b184746ac3e7
brgltd <brgltdxyz@gmail.com>
2024-11-14 18:56:25 -0300

    feat: replace deploy-anvil.sh with deploy-contracts.sh

--- contracts/deploy-anvil.sh
+++
@@ -1,5 +0,0 @@
-source .env
-
-forge script script/DeployRaceGridNFT.s.sol:DeployRaceGridNFT --rpc-url $LOCAL_ANVIL_RPC_URL --broadcast -vvvv
-
-forge script script/DeployLeaderboard.s.sol:DeployLeaderboard --rpc-url $LOCAL_ANVIL_RPC_URL --broadcast -vvvv
---
+++ contracts/deploy-contracts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source .env
+
+rpc_url=""
+
+if [[ $1 == "anvil" ]]; then
+  rpc_url=$LOCAL_ANVIL_RPC_URL
+elif [[ $1 == "taiko_testnet" ]]; then
+  rpc_url=$TAIKO_TESTNET_RPC_URL
+else
+  echo "invalid argument"
+  exit 1
+fi
+
+echo $rpc_url
+
+# forge script script/DeployRaceGridNFT.s.sol:DeployRaceGridNFT --rpc-url $rpc_url --broadcast -vvvv
+
+# forge script script/DeployLeaderboard.s.sol:DeployLeaderboard --rpc-url $rpc_url --broadcast -vvvv
