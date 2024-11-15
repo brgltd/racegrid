eb5897a9e48c719bc41db3ef9fbb564b53894f84
brgltd <brgltdxyz@gmail.com>
2024-11-15 11:11:37 -0300

    fix: handle leaderboard contract deployed to taiko testnet

--- README.md
+++ README.md
@@ -14,8 +14,8 @@ RaceGridNFT
 https://hekla.taikoscan.io/address/0x1c04808ee9d755f7b3b2d7fe7933f4aec8d8ee0e#code
 
 Leaderboad
-...
+https://hekla.taikoscan.io/address/0xbf640425d199d33b84e150dac80d2d961f63ad51#code
 
 ## Misc
 
-To see your NFT in metamask, please enable: Settings > Security and Privacy > Display NFT Media
+To see your NFT image in metamask, please import the NFT and enable: Settings > Security and Privacy > Display NFT Media
--- frontend/ethers.ts
+++ frontend/ethers.ts
@@ -1,21 +1,29 @@
 import { ethers } from "ethers";
-import { anvil } from "viem/chains";
+import { anvil, taikoHekla } from "viem/chains";
 import { Constants } from "./constants";
 import { leaderboardAbi } from "./abis";
-import { anvilChain } from "./chains";
-
-export const anvilProviderReadable = new ethers.providers.JsonRpcProvider(
-  anvil.rpcUrls.default.http[0],
-);
+import { anvilChain, taikoTestnetChain } from "./chains";
 
 export function getLeaderboardContract(chain: string) {
   if (chain === anvilChain.name) {
+    const anvilProviderReadable = new ethers.providers.JsonRpcProvider(
+      anvil.rpcUrls.default.http[0],
+    );
     return new ethers.Contract(
       Constants.Anvil.Leaderboard,
       leaderboardAbi,
       anvilProviderReadable,
     );
   }
-  // TODO: add taiko testnet
+  if (chain === taikoTestnetChain.name) {
+    const taikoTestnetProviderReadable = new ethers.providers.JsonRpcProvider(
+      taikoHekla.rpcUrls.default.http[0],
+    );
+    return new ethers.Contract(
+      Constants.TaikoTestnet.Leaderboard,
+      leaderboardAbi,
+      taikoTestnetProviderReadable,
+    );
+  }
   throw new Error("undefined chain");
 }
