1722c4654912e80b0b842b9b129040dd054ef8df
brgltd <brgltdxyz@gmail.com>
2024-11-15 08:32:48 -0300

    feat: deploy contracts to taiko testnet

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -5,4 +5,9 @@ export const Constants = {
     RaceGridNFT: "0x5FbDB2315678afecb367f032d93F642f64180aa3" as Address,
     Leaderboard: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512" as Address,
   },
+
+  TaikoTestnet: {
+    RaceGridNFT: "0x1C04808EE9d755f7B3b2d7fe7933F4Aec8D8Ee0e" as Address,
+    Leaderboard: "0xBF640425d199D33b84E150DAc80D2d961F63AD51" as Address,
+  },
 };
