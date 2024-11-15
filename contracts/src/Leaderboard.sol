13820752379e8c564d4877607f4e7768adfa9b60
brgltd <brgltdxyz@gmail.com>
2024-11-15 07:40:50 -0300

    docs: natspecify the contracts

--- contracts/src/Leaderboard.sol
+++ contracts/src/Leaderboard.sol
@@ -3,24 +3,34 @@ pragma solidity ^0.8.26;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title Leaderboard
+/// @notice Leaderboard entries for the Race Grid blockchain racing game.
 contract Leaderboard is Ownable {
+    /// @notice The results for each race.
     struct Result {
         address player;
         uint256 time;
         uint256 date;
     }
 
+    /// @notice The list of results.
     Result[] private results;
 
+    /// @notice Initialize the contract with caller as owner.
     constructor() Ownable(msg.sender) {}
 
     /// @notice Add new entry to the leaderboard.
+    /// @param player The address of the player.
+    /// @param time The duration of the race.
     // For hackathon or testnets it's okay to leave this open. For mainnet needs to be `onlyOwner`.
     function updateLeaderboard(address player, uint256 time) external {
         results.push(Result({player: player, time: time, date: block.timestamp}));
     }
 
     /// @notice Retrieve leaderboard results.
+    /// @param startIndex The start index, inclusive.
+    /// @param endIndex The end index, exclusive.
+    /// @return The array with the paginated results.
     /// @dev Using pagination to avoid OOG. Client is responsible for aggregation and sorting.
     function getResultsPaginated(uint256 startIndex, uint256 endIndex) external view returns (Result[] memory) {
         Result[] memory resultsCopy = results;
@@ -34,6 +44,7 @@ contract Leaderboard is Ownable {
     }
 
     /// @notice Delete one entry from the leaderboard.
+    /// @param targetIndex The index to delete.
     /// @dev Mainly used for testing.
     function deleteLeaderboardEntry(uint256 targetIndex) external onlyOwner {
         Result[] memory resultsCopy = results;
--- contracts/src/RaceGridNFT.sol
+++ contracts/src/RaceGridNFT.sol
@@ -3,16 +3,23 @@ pragma solidity ^0.8.26;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/// @title RaceGridNFT
+/// @notice NFTs for the Race Grid blockchain racing game.
 contract RaceGridNFT is ERC721 {
+    /// @notice The token id counter.
     uint256 private tokenId;
 
+    /// @notice Map an token id to token URI.
     mapping(uint256 id => string uri) public tokenIdToURI;
 
-    /// @notice Last token owned by an address.
+    /// @notice Map an address to token URI.
+    /// @dev Last token owned by an address.
     mapping(address user => string uri) public userToTokenURI;
 
+    /// @notice Initialize the NFT.
     constructor() ERC721("RaceGrid", "RCG") {}
 
+    /// @param uri The NFT URI.
     function mint(string memory uri) public {
         tokenIdToURI[tokenId] = uri;
         userToTokenURI[msg.sender] = uri;
@@ -20,6 +27,9 @@ contract RaceGridNFT is ERC721 {
         ++tokenId;
     }
 
+    /// @notice Returns the token URI for a token id.
+    /// @param id The token id.
+    /// @return The token URI.
     function tokenURI(uint256 id) public view override returns (string memory) {
         return tokenIdToURI[id];
     }
